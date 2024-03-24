0ab76e5e6c98d8daeb6aca21af04374a2d5674a2
HuyenPTN <huyenptn182769@gmail.com>
2024-03-25 05:59:25 +0700

    Update Search Book - Admin Page

--- Pages/Admin/Books/Index.cshtml.cs
+++ Pages/Admin/Books/Index.cshtml.cs
@@ -6,42 +6,45 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using BookStore.Models;
+using static Microsoft.Extensions.Logging.EventSource.LoggingEventSource;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace BookStore.Pages.Admin.Books
 {
-	public class IndexModel : PageModel
-	{
-		private readonly BookStoreContext context;
+    public class IndexModel : PageModel
+    {
+        private readonly BookStoreContext context;
 
-		public IndexModel()
-		{
-			context = new BookStoreContext();
-		}
+        public IndexModel()
+        {
+            context = new BookStoreContext();
+        }
 
-		public List<SubCategory> SubCategories { get; set; }
-		public List<Book> Books { get; set; }
-		public string Keywords { get; set; }
+        public List<SubCategory> SubCategories { get; set; }
+        public List<Book> Books { get; set; }
+        public string Keywords { get; set; }
 
-		public void OnGet(string keyword)
-		{
-			Keywords = keyword;
-			SubCategories = context.SubCategories.ToList();
+        public void OnGet(string keyword)
+        {
+            Keywords = keyword;
+            SubCategories = context.SubCategories.ToList();
 
-			if (string.IsNullOrEmpty(keyword))
-			{
-				Books = context.Books
-					.Include(x => x.Publisher)
-					.OrderByDescending(x => x.BookId)
-					.ToList();
-			}
-			else
-			{
-				Books = context.Books
-					.Include(x => x.Publisher)
-					.Where(x => x.BookName.Contains(keyword))
-					.OrderByDescending(x => x.BookId)
-					.ToList();
-			}
-		}
-	}
+            if (string.IsNullOrEmpty(keyword))
+            {
+                Books = context.Books
+                    .Include(x => x.Publisher)
+                    .OrderByDescending(x => x.BookId)
+                    .ToList();
+            }
+            else
+            {
+                Books = context.Books
+                    .Include(x => x.Publisher)
+                    .Where(x => x.BookName.Contains(keyword)
+                    || x.Author.Contains(keyword))
+                    .OrderByDescending(x => x.BookId)
+                    .ToList();
+            }
+        }
+    }
 }
